a01df0d4afcccdc2936051364f7b26c81b2a1b57
mr-kasper <rachidwork5050@gmail.com>
2024-09-19 17:21:09 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Jokes from './components/Jokes';
+// import Posts from './components/Posts';
+
+const App = () => {
+  return (
+    <main className="App">
+      <h1>useAxios Hooks</h1>
+      <Jokes />
+      {/* <Posts /> */}
+    </main>
+  );
+};
+export default App;
